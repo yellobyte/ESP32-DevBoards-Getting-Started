405375e96b577951a72fe86268024761503f1afa
Thomas Jentzsch <76921504+yellobyte@users.noreply.github.com>
2022-10-16 23:11:02 +0200

    Initial commit

--- boards/ESP32-DevKit-V1_(Espressif)/examples/Test-DevKit-V1-Semaphore-1/src/main.cpp
+++ boards/ESP32-DevKit-V1_(Espressif)/examples/Test-DevKit-V1-Semaphore-1/src/main.cpp
@@ -1,27 +1,46 @@
+/*
+  Test Semaphore-1
+
+  Semaphores are useful in many situations, especially for thread synchronisation and control.
+	This example demonstrates the usage of a semaphore for triggering a waiting threat.
+	
+	The first call to xSemaphoreTake() in loop() is successful, the next calls will make the loop pause because 
+	a boolean semaphore can only be taken once. Only when the ISR in the background periodically releases the semaphore
+  the loop can obtain the semaphore again and carry on.
+	
+  More infos about semaphores & their usage: "https://www.freertos.org/a00113.html".
+
+  Last updated 2020-07-21, ThJ <yellobyte@bluewin.ch>
+*/
+
 #include <Arduino.h>
 
-#define TIMEOUT_MS 3000    // 5s
-hw_timer_t * timer;
+#define TIMEOUT_MS 3000    // 3000ms
 
-SemaphoreHandle_t syncSemaphore;
+hw_timer_t        *timer;
+SemaphoreHandle_t  syncSemaphore;
 
 void IRAM_ATTR onTimer() {
-  xSemaphoreGiveFromISR(syncSemaphore, NULL);
+  xSemaphoreGiveFromISR(syncSemaphore, NULL);       // release semaphore
 }
 
 void setup() {
-
   Serial.begin(115200);
   syncSemaphore = xSemaphoreCreateBinary();
-  timer = timerBegin(0, 80, true);                  // Use 1st timer (0), prescaler 80, counting upwards (true)
-  timerAttachInterrupt(timer, &onTimer, true);      // Call onTimer on timer alarm, generated on edge (true)
-  timerAlarmWrite(timer, TIMEOUT_MS * 1000, true);  // Set Timeout: 3s in our case, reload
+  timer = timerBegin(0, 80, true);                  // use 1st timer (0), set prescaler to 80, count upwards
+  timerAttachInterrupt(timer, &onTimer, true);      // call onTimer on timer alarm, generated on edge (true)
+  timerAlarmWrite(timer, TIMEOUT_MS * 1000, true);  // set timeout, reload timer when expired
   timerAlarmEnable(timer);
+  Serial.println();
+  Serial.println("Semaphore created, timer started. setup() has finished.");
 }
 
 void loop() {
   static int count = 0;
-  xSemaphoreTake(syncSemaphore, portMAX_DELAY);     // portMAX_DELAY makes sure we wait indefinitely
-  Serial.print("Semaphore was released ");
+  xSemaphoreTake(syncSemaphore, portMAX_DELAY);     // wait to obtain semaphore when it becomes available
+  Serial.print("Semaphore was released. Loop count = ");
   Serial.println(++count);
+	//
+	// do some work...
+	//
 }
