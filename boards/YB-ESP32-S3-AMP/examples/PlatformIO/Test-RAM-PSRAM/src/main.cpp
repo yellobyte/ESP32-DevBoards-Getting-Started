b1ec801b9ec9d61a0f03980e2c033031d8dd9731
Thomas Jentzsch <76921504+yellobyte@users.noreply.github.com>
2025-01-12 18:35:48 +0100

    example updated

--- boards/YB-ESP32-S3-AMP/examples/PlatformIO/Test-RAM-PSRAM/src/main.cpp
+++ boards/YB-ESP32-S3-AMP/examples/PlatformIO/Test-RAM-PSRAM/src/main.cpp
@@ -1,15 +1,30 @@
 /*
-  Test RAM & PSRAM
+  Test_RAM
 
-  Shows available RAM/PSRAM on the YB-ESP32-S3-AMP dev board and tests the PSRAM.
-  Depending on the amount of memory allocated with malloc() it will come from RAM or PSRAM.
+  1) Shows available RAM/PSRAM on the YB-ESP32-S3-AMP dev board.
+  2) Allocates as many memory chunks as possible and shows where the allocated memory comes from.
+     According to the ESP32-S3 spec the address mapping structure is as follows:
+     PSRAM:        0x3C000000-0x3DFFFFFF, 0x42000000-0x43FFFFFF 
+     SRAM:         0x3FC88000-0x3FCFFFFF, 0x40370000-0x403DFFFF
+     RTC FAST MEM: 0x600FE000-0x600FFFFF
+  3) When almost no memory is left then even serial output gets affected.
+  4) Finally all allocated memory is released.
 
-  Last updated 2024-11-29, ThJ <yellobyte@bluewin.ch>
+  The memory allocated will come from RAM or PSRAM as you can see on the address returned by malloc().
+
+  Last updated 2025-01-12, ThJ <yellobyte@bluewin.ch>
 */
 
 #include <Arduino.h>
 
-uint8_t *buf;
+#define ORIGIN(p)                                                                        \
+        ((p >= (uint8_t*)0x3C000000 && p <= (uint8_t*)0x3DFFFFFF) ||                     \
+         (p >= (uint8_t*)0x42000000 && p <= (uint8_t*)0x43FFFFFF)) ? "PSRAM" :           \
+         ((p >= (uint8_t*)0x3FC88000 && p <= (uint8_t*)0x3FCFFFFF) ||                    \
+          (p >= (uint8_t*)0x40370000 && p <= (uint8_t*)0x403DFFFF)) ? "SRAM" :           \
+          (p >= (uint8_t*)0x600FE000 && p <= (uint8_t*)0x600FFFFF) ? "RTC FAST MEM" : ""
+
+uint8_t *p, *pArray[30], cnt = 0;
 
 void logRam(bool showRam = true, bool showPsram = true) 
 {
@@ -28,39 +43,64 @@ void logRam(bool showRam = true, bool showPsram = true)
   Serial.println();
 }
 
-bool allocRAM(size_t size)
+uint8_t *allocMaxRAM(size_t startSize)
 {
+  uint8_t *p;
+  size_t i, size;
+
+  for (size = startSize, p = NULL; size > 0 && p == NULL; size--) {
+    p = (uint8_t*)malloc(size);
+  }
+  if (p == NULL) {
+    Serial.printf("-> error !\n");
+    return NULL;
+  }
+  Serial.printf("-> malloc(%d): success, p = %x(...%x), %s\n", size, p, p + size, ORIGIN(p));
+
+  // very simple check of every single byte in allocated memory
+  // write pattern
+  for (i = 0; i < size; i++) {
+    p[i] = i % 0xFF;
+  }
+    // read pattern
+  for (i = 0; i < size; i++) {
+    if (i % 0xFF != p[i]) {
+      Serial.printf("w/r error at p[%d]\n", i);
+      break;
+    }
+  }
+
+  return p;
+} 
+
+uint8_t *allocRAM(size_t size)
+{
+  uint8_t *p;
   size_t i;
 
   Serial.printf("-> malloc(%d): ", size);
-  buf = (uint8_t*)malloc(size);
-  if (buf != NULL) {
-    // simple check of every single byte position
-    for (i = 0; i < size; i++) {
-      buf[i] = i % 0xFF;
-      if (i % 0xFF != buf[i]) {
-        Serial.printf("w/r error at buf[%d]\n", i);
-        break;
-      }
-    }
-    if (i == size) {
-      Serial.printf("success, buf = %x\n", buf);
-      return true;
-    }	
-  } 
-  else {
+  p = (uint8_t*)malloc(size);
+  if (p == NULL) {
     Serial.printf("error !\n");
+    return NULL;
   }
-  return false;
-}  
+  Serial.printf("success, p = %x(...%x), %s\n", p, p + size, ORIGIN(p));
 
-void freeBuf()
-{
-  if (buf != NULL) {
-    Serial.printf("-> free(buf=%x)\n", buf); 
-    free(buf);
+  // very simple check of every single byte in allocated memory
+  // write pattern
+  for (i = 0; i < size; i++) {
+    p[i] = i % 0xFF;
   }
-}
+    // read pattern
+  for (i = 0; i < size; i++) {
+    if (i % 0xFF != p[i]) {
+      Serial.printf("w/r error at p[%d]\n", i);
+      break;
+    }
+  }
+
+  return p;
+}  
 
 void setup() {
   Serial.begin(115200);
@@ -75,26 +115,26 @@ void setup() {
   Serial.print("ESP32 SDK: ");        Serial.println(ESP.getSdkVersion());
   Serial.print("ESP32 CPU FREQ: ");   Serial.print(getCpuFrequencyMhz());             Serial.println(" MHz");
   Serial.print("ESP32 APB FREQ: ");   Serial.print(getApbFrequency() / 1000000.0, 1); Serial.println(" MHz (Advanced Peripheral Bus)");
-  Serial.print("ESP32 FLASH SIZE: "); Serial.print(ESP.getFlashChipSize());           Serial.println(" Byte");
+  Serial.print("ESP32 FLASH SIZE: "); Serial.print(ESP.getFlashChipSize());           Serial.println(" Byte\n");
 
   logRam();
-
-  Serial.println("----- allocation of 1.5MB memory ----->");
-  allocRAM(3 * 1024 * 516);
-  logRam();
-  freeBuf();
-  logRam();
-
-  Serial.println("----- allocation of 100kB memory ----->");
-  allocRAM(100 * 1024);
-  logRam();
-  freeBuf();
+  Serial.println("----- allocate biggest possible chunk, please wait ----->");
+  p = allocMaxRAM(2 * 1024 * 1024);   // 2MB start size
+  if (!p) return;
+  pArray[cnt++] = p;
   logRam();
 
-  Serial.println("----- allocation of 1kB memory ----->");
-  allocRAM(1024);
-  logRam();
-  freeBuf();
+  Serial.println("----- allocate more chunks of memory ----->");
+  for (;ESP.getMaxAllocHeap() > 0;) {
+    p = allocRAM(ESP.getMaxAllocHeap());
+    if (!p) break;
+    pArray[cnt++] = p;
+    logRam();
+  }
+
+  Serial.println("\n----- free all allocated memory ----->");
+  for (uint8_t i = cnt; i > 0; i--)
+    free(pArray[i - 1]);
   logRam();
 
   Serial.println("\nAll done."); 
