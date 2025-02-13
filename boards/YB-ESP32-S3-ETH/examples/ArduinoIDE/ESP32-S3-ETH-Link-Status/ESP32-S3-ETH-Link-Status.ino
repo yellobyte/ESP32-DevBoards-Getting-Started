ce4a925d1c02cc05e39e53a90c0ccb74bfe14d88
Thomas Jentzsch <76921504+yellobyte@users.noreply.github.com>
2025-02-13 17:11:10 +0100

    Update ESP32-S3-ETH-Link-Status.ino

--- boards/YB-ESP32-S3-ETH/examples/ArduinoIDE/ESP32-S3-ETH-Link-Status/ESP32-S3-ETH-Link-Status.ino
+++ boards/YB-ESP32-S3-ETH/examples/ArduinoIDE/ESP32-S3-ETH-Link-Status/ESP32-S3-ETH-Link-Status.ino
@@ -5,18 +5,15 @@
   The status LED should be "ON" when an ethernet cable is properly connected between board and a switch.
   Getting an IP address via DHCP is not tested.
 
-  Last updated 2023-06-05, ThJ <yellobyte@bluewin.ch>
+  Last updated 2025-02-13, ThJ <yellobyte@bluewin.ch>
 */
 
 #include <Arduino.h>
 #include <Ethernet.h>
 
-#define GPIO_W5500_CS    14                     // onboard W5500 CS chip select pin connected to GPIO14
-#define GPIO_STATUS_LED  47                     // onboard status LED connected to GPIO47
-
 void setup() {
-  pinMode(GPIO_STATUS_LED, OUTPUT);
-  digitalWrite(GPIO_STATUS_LED, LOW);           // status LED off  
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);           // status LED off  
   
   Serial.begin(115200);
   // Port 'USB' (directly attached to ESP32-S3 chip !) will be gone for a few seconds after resetting the board, 
@@ -27,7 +24,7 @@ void setup() {
 #endif  
 
   delay(1000);  
-  Ethernet.init(GPIO_W5500_CS);
+  Ethernet.init(W5500_SS);
 }
 
 void loop() {
@@ -36,15 +33,15 @@ void loop() {
   switch (status) {
     case Unknown:
       Serial.println("Unknown");
-      digitalWrite(GPIO_STATUS_LED, LOW);       // status LED off  
+      digitalWrite(LED_BUILTIN, LOW);       // status LED off  
       break;
     case LinkON:
       Serial.println("ON");
-      digitalWrite(GPIO_STATUS_LED, HIGH);      // status LED on  
+      digitalWrite(LED_BUILTIN, HIGH);      // status LED on  
       break;
     case LinkOFF:
       Serial.println("OFF");
-      digitalWrite(GPIO_STATUS_LED, LOW);       // status LED off  
+      digitalWrite(LED_BUILTIN, LOW);       // status LED off  
       break;
   }
   delay(1000);
