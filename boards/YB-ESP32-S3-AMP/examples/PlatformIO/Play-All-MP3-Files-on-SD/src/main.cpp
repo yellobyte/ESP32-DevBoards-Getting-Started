7d08d360adc19c915e2ff0ed04838b1766756543
Thomas Jentzsch <76921504+yellobyte@users.noreply.github.com>
2025-03-02 06:39:03 +0100

    Update main.cpp

--- boards/YB-ESP32-S3-AMP/examples/PlatformIO/Play-All-MP3-Files-on-SD/src/main.cpp
+++ boards/YB-ESP32-S3-AMP/examples/PlatformIO/Play-All-MP3-Files-on-SD/src/main.cpp
@@ -11,7 +11,7 @@
   GPIO47 (onboard LED IO47) with the amps SD_MODE pin. Setting GPIO47 to LOW (LED off) will shut down (mute) the amps
   and setting GPIO47 to HIGH (LED on) will activate the amps.
 
-  Last updated 2025-02-13, ThJ <yellobyte@bluewin.ch>
+  Last updated 2025-03-02, ThJ <yellobyte@bluewin.ch>
 */
 
 #include <Arduino.h>
@@ -75,9 +75,10 @@ void setup()
 
   Serial.begin(115200);
 #if ARDUINO_USB_CDC_ON_BOOT == 1
-  // we continue only when serial port becomes available: only needed for board revision 3.x
-  while (!Serial);     	
+  // we continue only when serial port becomes available, uncomment if needed
+  //while (!Serial) delay(1);   
 #endif	
+  delay(5000);  	
   Serial.println();
   Serial.println("running example \"Play-All-MP3-Files-on-SD\":");
 
