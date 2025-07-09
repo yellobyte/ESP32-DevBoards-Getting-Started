0a628b3690e47b923687462922e8c43f3887c16d
Thomas Jentzsch <76921504+yellobyte@users.noreply.github.com>
2025-07-10 01:20:01 +0200

    example updated

--- boards/ESP32-DevKit-V1/examples/PlatformIO/Test-DevKit-V1-Watchdog-2/src/main.cpp
+++ boards/ESP32-DevKit-V1/examples/PlatformIO/Test-DevKit-V1-Watchdog-2/src/main.cpp
@@ -3,7 +3,7 @@
 
   Using an IRQ routine to implement a watchdog instead of framework provided watchdogs. 
 	
-  Last updated 2020-04-06, ThJ <yellobyte@bluewin.ch>
+  Last updated 2025-07-10, ThJ <yellobyte@bluewin.ch>
 */
 
 #include <Arduino.h>
@@ -22,10 +22,16 @@ void setup() {
   Serial.println();
   Serial.println("running setup");
 
-  timer = timerBegin(0, 80, true);                      // use 1st timer (0), set prescaler to 80, count upwards (true)
-  timerAttachInterrupt(timer, &resetModule, false);     // call resetModule() on timer alarm, generated on LEVEL (false)
-  timerAlarmWrite(timer, wdtTimeout * 1000, false);     // set watchdog timeout (5000000us = 5s in our case), no reload
-  timerAlarmEnable(timer);                              // enable timer interrupt
+  // Arduino ESP32 API 2.x   
+  // timer = timerBegin(0, 80, true);                      // use 1st timer (0), set prescaler to 80, count upwards (true)
+  // timerAttachInterrupt(timer, &resetModule, false);     // call resetModule() on timer alarm, generated on LEVEL (false)
+  // timerAlarmWrite(timer, wdtTimeout * 1000, false);     // set watchdog timeout (5000000us = 5s in our case), no reload
+  // timerAlarmEnable(timer);                              // enable timer interrupt
+
+  // Arduino ESP32 API 3.x                                             
+  timer = timerBegin(1000000);                          // set timer clock to 1uSec (1MHz)  
+  timerAttachInterrupt(timer, &resetModule);            // on timer alarm call function resetModule 
+  timerAlarm(timer, wdtTimeout * 1000, true, 0);        // set timeout, reload timer when expired (true)
 }
 
 void loop() {
